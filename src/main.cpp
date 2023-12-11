9c80848255062ef77675a40a490d86ca8391c989
Владислав <nondefraudat@gmail.com>
2023-12-11 15:56:32 +0300

    Inversions counting

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,27 @@
 #include "sorts/mergesort.inc"
+#include "misc/countinversions.inc"
 #include <iostream>
 
 using namespace std;
 
 int main() {
 	int ok[] = { 2, 3, 5, -1, 3, 4, 5, 8, 7, 1 };
+	const size_t size(_countof(ok));
+
+	cout << "source array: { ";
 	for (int i : ok) {
-		cout << i << " ";
+		cout << i << ", ";
 	}
-	cout << endl;
-	int* kk = mergeSort<int>(ok, _countof(ok));
-	for (int i = 0; i < _countof(ok); i++) {
-		cout << kk[i] << " ";
+	cout << "}\ninversions count: " <<
+			countInversions<int>(ok, size) << endl;
+	
+	int* kk = mergeSort<int>(ok, size);
+	cout << "sorted array: { ";
+	for (int i = 0; i < size; i++) {
+		cout << kk[i] << ", ";
 	}
-	cout << endl;
+	cout << "}\ninversions count: " <<
+		countInversions<int>(kk, size) << endl;
+
 	return 0;
 }
